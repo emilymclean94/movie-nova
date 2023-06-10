a4e5bd715a725ac0bfd86cbbbcc3886d88d2bc4f
Emily McLean <emilymclean93@gmail.com>
2023-06-10 17:41:15 -0400

    added queries js to client folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      name
+      username
+      email
+      genre
+      bio
+      watched {
+        _id
+        posterImg
+        title
+        releaseDate
+        description
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+query me {
+ me {
+    _id
+    firstname
+    username
+    email
+    genre
+    bio
+    watched {
+      _id
+      posterImg
+      title
+      releaseDate
+      description
+    }
+  }
+}
+`;
+
+export const QUERY_MOVIES = gql`
+  query getMovies {
+    movie {
+        _id
+        posterImg
+        title
+        releaseDate
+        description
+    }
+  }
+`;
+
+export const QUERY_SINGLE_MOVIE = gql`
+  query getSingleMovie($movieId: ID!) {
+    movie(movieId: $movieId) {
+        _id
+        posterImg
+        title
+        releaseDate
+        description
+    }
+  }
+`;
