cd7b0e31488adbe31e4772841e5c4953a82c67b5
EGARRISXN <egarrisxn@gmail.com>
2023-06-10 15:24:25 -0400

    some BASIC framkework for frontend. will change the file structure right now.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+
+function Footer() {
+  
+  return (
+    <Container className="footer">
+      <h1>Footer</h1>
+      <h1>Goes</h1>
+      <h1>Here</h1>
+    </Container>
+  )
+}
+
+export default Footer;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React  from "react";
+import { Container } from "react-bootstrap";
+
+
+function NavBar() {
+
+  return (
+    <Container className="navbar">
+      <h1>Navbar</h1>
+      <h1>Goes</h1>
+      <h1>Here</h1>
+    </Container>
+  )
+}
+
+export default NavBar;
