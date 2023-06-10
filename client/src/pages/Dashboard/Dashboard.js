c0365600260959fe94773e8da4e86861f2d3f8f1
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-06-10 19:12:24 -0400

    added login functionality

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,42 +1,42 @@
-import React from "react";
-import { useParams } from 'react-router';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../../utils/queries';
-import Auth from '../../utils/auth';
-import { Container } from "react-bootstrap";
+// import React from "react";
+// import { useParams } from 'react-router';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_USER, QUERY_ME } from '../../utils/queries';
+// import Auth from '../../utils/auth';
+// import { Container } from "react-bootstrap";
 
 
-function Dashboard() {
-    const { username: userParam } = useParams();
+// function Dashboard() {
+//     const { username: userParam } = useParams();
   
-    const { data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-      variables: { username: userParam },
-    }); //loading was next to data
+//     const { data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+//       variables: { username: userParam },
+//     }); //loading was next to data
   
-    const user = data?.me || data?.user || {};
-    // navigate to personal profile page if username is yours
+//     const user = data?.me || data?.user || {};
+//     // navigate to personal profile page if username is yours
 
 
-  return (
-    <>
-    {Auth.loggedIn() ? (
-    <Container className="dashboard-body" id="dashboard">
-      <Container className="dashboard-content">
-        <h1>Welcome {`${user.name}`} to your Dashboard!</h1>
-        <div>
-        <Username>{`${user.username}`}</Username>
-          <FavoriteGenre>{`${user.genre}`}</FavoriteGenre>
-          <Bio>generate info</Bio>
-          <UpdateProfile>Update Profile</UpdateProfile>
-        </div>
-      </Container>
-    </Container>) : (
-      <h4>
-      You need to be logged in to see this. Use the navigation links above to
-      sign up or log in!
-    </h4>)}
-</>
-  );
-}
+//   return (
+//     <>
+//     {Auth.loggedIn() ? (
+//     <Container className="dashboard-body" id="dashboard">
+//       <Container className="dashboard-content">
+//         <h1>Welcome {`${user.name}`} to your Dashboard!</h1>
+//         <div>
+//         <Username>{`${user.username}`}</Username>
+//           <FavoriteGenre>{`${user.genre}`}</FavoriteGenre>
+//           <Bio>generate info</Bio>
+//           <UpdateProfile>Update Profile</UpdateProfile>
+//         </div>
+//       </Container>
+//     </Container>) : (
+//       <h4>
+//       You need to be logged in to see this. Use the navigation links above to
+//       sign up or log in!
+//     </h4>)}
+// </>
+//   );
+// }
 
-export default Dashboard;
+// export default Dashboard;
