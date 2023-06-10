86a1095ab1ef0368469e1e2eb79e615331c58c5f
Emily McLean <emilymclean93@gmail.com>
2023-06-10 12:30:14 -0400

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+const movieSchema = require("./Movie");
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, 'Must match an email address!'], },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 5 },
+    genre: { type: String},
+    bio: { type: String },
+    myList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Movie',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return `${this.friends.length}`;
+  })
+
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
