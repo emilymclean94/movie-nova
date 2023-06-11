4b83c6f847f6b9b5068647e3ef84788dbf730ead
keelyybug <keelysherman00@gmail.com>
2023-06-11 12:39:46 -0400

    reconfigured hero emotion to css

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,126 +1,39 @@
 import React from "react";
 import blue from "../Assets/blue.gif";
 
-const commonStyles = {
-  padding: "1rem",
-  width: "100%",
-};
-
 const HeroLeft = () => {
 
-  const ImageContainer = styled(Box)`
-    display: flex;
-    justify-content: center;
-    height: 80vh;
-
-    @media (max-width: 767px) {
-      height: 50vh;
-      margin-bottom: 3.5rem;
-    }
-  `;
-
-  const IMG = styled.img`
-    max-height: 100%;
-    max-width: 100%;
-  `;
-
   return (
-    <ImageContainer sx={commonStyles}>
-      <IMG src={blue} className="blue" alt="cool" />
-    </ImageContainer>
+    <div className='image-container' sx={commonStyles}>
+      <img src={blue} className="blue" alt="cool" />
+    </div>
   );
 };
 
 const HeroRight = () => {
 
-  const TextContainer = styled(Box)`
-    display: flex;
-    justify-content: flex-start;
-    height: 80vh;
-
-    @media (max-width: 767px) {
-      margin-top: 3.0rem;
-      height: 20vh;
-    }
-  `;
-
-  const SubTitle = styled.h3`
-    text-align: left;
-    color: #808080;
-    margin-top: 0.5rem;
-    margin: 0;
-    font-size: 2rem;
-
-    @media (max-width: 767px) {
-      margin-top: 0.4rem;
-      font-size: 1rem;
-    }
-  `;
-
-  const Title = styled.h3`
-    text-align: left;
-    color: white;
-    margin: 0;
-    font-size: 6rem;
-
-    @media (max-width: 767px) {
-      font-size: 1.5rem;
-    }
-  `;
-
   return (
-    <TextContainer sx={commonStyles}>
-      <Stack>
-        <SubTitle>
+    <div className="text-container">
+        <h3 className='sub-title'>
           INTO THE
-        </SubTitle>
-        <Title>
+        </h3>
+        <h3 className='title'>
           STREAMVERSE
-        </Title>
-      </Stack>
-    </TextContainer>
+        </h3>
+    </div>
   );
 };
 
 const Hero = () => {
-  const AppContainer = styled(Box)`
-    display: flex;
-    flex-direction: column-reverse;
-    height: 100%;
-    background-color: black;
-
-    @media (min-width: 768px) {
-      flex-direction: row;
-    }
-  `;
-
-  const LeftContainer = styled(Box)`
-    background-color: black;
-    width: 100%;
-
-    @media (min-width: 768px) {
-      width: 50%;
-    }
-  `;
-
-  const RightContainer = styled(Box)`
-    background-color: black;
-    width: 100%;
-
-    @media (min-width: 768px) {
-      width: 50%;
-    }
-  `;
-
   return (
-    <AppContainer>
-      <LeftContainer>
+    <div className='app-container'>
+      <div className='left-container'>
         <HeroLeft />
-      </LeftContainer>
-      <RightContainer>
+      </div>
+      <div classNAme='right-container'>
         <HeroRight />
-      </RightContainer>
-    </AppContainer>
+      </div>
+    </div>
   );
 };
 
