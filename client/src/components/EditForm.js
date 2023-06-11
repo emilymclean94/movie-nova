6f7d79aa474292f7e9f6ea46f95d3c86faf2dfe0
Emily McLean <emilymclean93@gmail.com>
2023-06-11 02:44:26 -0400

    coded initial user update functino on client side. Deleted input.js as it was not needed

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -1,108 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { useMutation } from '@apollo/client';
 import { UPDATE_USER } from '../utils/mutations'
-import Input from './Input';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import { useParams } from 'react-router';
-import { useQuery } from '@apollo/client';
+import { Form } from 'react-bootstrap';
+import Auth from '../utils/auth';
+// import { QUERY_USER, QUERY_ME } from '../utils/queries';
+// import { useParams } from 'react-router';
+// import { useQuery } from '@apollo/client';
 
 const EditForm = () => {
-  const { username: userParam } = useParams();
-
-  const { data, loading, error } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  }); //loading was next to data
-
-  const [updateUser] = useMutation(UPDATE_USER);
-
-  const [userInfo, setUserInfo] = useState({
+  const [editUser, setEditUser] = useState({
     name: '',
     username: '',
     genre: '',
     bio: '',
-  });
+});
 
-  useEffect(() => {
-    if (data) {
-      const user = userParam ? data.user : data.me;
-      setUserInfo(user);
-    }
-  }, [data, userParam]);
-  console.log(userInfo);
-  
-  const handleUpdateUser = async () => {
-    try {
-      await updateUser({ variables: { input: userInfo } });
-      // Perform any necessary UI updates or refetching of data
-    } catch (error) {
-      console.error('Error updating user:', error);
-    }
-  };
+  const [updateUser] = useMutation(UPDATE_USER);
 
-  const handleInput = (event) => {
-    const { name, value } = event.target;
-    setUserInfo((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (event) => {
+  const handleUpdateUser = async (event) => {
     event.preventDefault();
-    console.log('Form submitted');
-    handleUpdateUser();
+    const { name, value } = event.target;
+
+    setEditUser({
+      ...editUser,
+      [name]: value,
+    });
   };
-  if (loading) {
-    return <p>Loading...</p>;
-  }
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-  if (!data || (!userParam && !data.me) || (userParam && !data.user)) {
-    return <p>User not found.</p>;
-  }
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(editUser);
 
+    try {
+      const { data } = await updateUser({
+        variables: { ...editUser },
+      });
+      
+      Auth.login(data.addUser.token);
+    } catch (e) {
+        console.error(e);
+    }
+  };
 
   return (
     <>
-      <h1>{userInfo.name}</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
+      <h1>Update Your Information</h1>
+      <form onSubmit={handleFormSubmit}>
+        <Form.Group>
+          <input
           name="name"
           type="text"
-          value={userInfo.name}
           placeholder="Updated your name"
-          handleInput={handleInput}
-        />
-        <br />
-        <Input
+          onChange={handleUpdateUser}
+          value={editUser.name}
+          />
+          </Form.Group>
+        <Form.Group>
+          <input 
           name="username"
           type="text"
-          value={userInfo.username}
           placeholder="Update Username"
-          handleInput={handleInput}
-        />
-        <br />
-        <Input
+          onChange={handleUpdateUser}
+          value={editUser.username}
+          />
+        </Form.Group>
+        <Form.Group>
+          <input
           name="genre"
           type="text"
-          value={userInfo.genre}
           placeholder="Update Genre"
-          handleInput={handleInput}
-        />
-        <br />
-        <Input
+          onChange={handleUpdateUser}
+          value={editUser.genre}
+          />
+        </Form.Group>
+        <Form.Group>
+          <input
           name="bio"
           type="text"
-          value={userInfo.bio}
           placeholder="Update Bio"
-          handleInput={handleInput}
-        />
-        <br />
-        <input className='btn btn-dark' type="submit" value="Update" />
+          onChange={handleUpdateUser}
+          value={editUser.bio}
+          />
+        </Form.Group>
+        <input className='btn btn-dark my-3' type="submit" value="Update" />
       </form>
     </>
   );
 }
 export default EditForm;
+
