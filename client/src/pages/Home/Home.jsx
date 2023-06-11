c0e98c249af6631445f79971f62b6474f51d394a
Emily McLean <emilymclean93@gmail.com>
2023-06-11 15:04:20 -0400

    added hero img, title, and basic styling to home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Hero from '../../Assets/homeGif.gif'
+import './home.css'
 
 const Home = () => {
 
   return (
 
-    <Container className="home-body" id="home">
-
-      <Container className="home-content">
-        <h1>Home</h1>
-      </Container>
-      
+    <Container className="home-body d-flex flex-row flex-wrap align-content-center bg-black" id="home">
+        <Col className="col-lg-6 image-container">
+        <img 
+        src={Hero}
+        />
+        </Col>
+        <Col className="col-lg-6">
+        <div className="text-container d-flex flex-wrap flex-column align-items-end m-2">
+        <h1 className='display-3 sub-title'>
+          INTO THE
+        </h1>
+        <h1 className='display-1 title'>
+          STREAMVERSE
+        </h1>
+    </div>
+        </Col>
     </Container>
     
   )
