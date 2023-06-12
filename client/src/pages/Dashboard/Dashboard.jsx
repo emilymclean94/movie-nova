6379814fb0c3c5d4812d09758a5b586191e42b73
keelyybug <keelysherman00@gmail.com>
2023-06-12 02:22:33 -0400

    styled my watched as much as i could- wouldnt allow me to round table corners

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
   const user = data?.me || data?.user || {};
 
   return (
-    <Container fluid>
+    <Container className="dashboard" fluid>
       <Row>
         {Auth.loggedIn() ? (
           <>
