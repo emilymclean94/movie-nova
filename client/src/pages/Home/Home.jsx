069f9ac3c3fc13d4918f2f93b76b7039864da984
Emily McLean <emilymclean93@gmail.com>
2023-09-11 12:32:35 -0400

    hero gif and title centered on mobile view

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,22 +3,24 @@ import { Container, Col } from "react-bootstrap";
 import Intro from "./Intro";
 import Features from "./Features";
 import Trust from "./Trust";
-import Maybe from "../../Assets/24OD.gif";
+import Hero from "../../Assets/24OD.gif";
 // import Hero from "../../assets/homeGif.gif";
 import "./home.css";
+
+
 
 const Home = () => {
   return (
     <div className="home-background">
       <Container
-        className="home-body d-flex flex-row flex-wrap align-content-center bg-"
+        className="home-body d-flex"
         id="home"
       >
-        <Col className="col-lg-6 image-container">
-          <img src={Maybe} alt="hero" />
+        <Col className="col-lg-6 col-sm-12 image-container">
+          <img className='hero-image' src={Hero} alt="hero gif of astronaut spinning around"/>
         </Col>
         <Col className="col-lg-6">
-          <div className="text-container d-flex flex-wrap flex-column align-items-end m-2">
+          <div className="text-container">
             <h1 className="display-3 sub-title">INTO THE</h1>
             <h1 className="display-1 title">STREAMVERSE</h1>
           </div>
@@ -34,3 +36,9 @@ const Home = () => {
 };
 
 export default Home;
+
+// home-body
+  // d-flex flex-row flex-wrap align-content-center
+
+// text-container
+  //d-flex flex-wrap flex-column align-items-end m-2
